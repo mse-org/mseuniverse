1a0e6e71dd44d3318cce58a583618095d257d4cd
Martin Schreiber <mse00000@gmail.com>
2011-11-05 14:17:11 +0100

    * MSEgit added missing fiels.

---
+++ tools/msegit/commitdiffform.pas
@@ -0,0 +1,36 @@
+{ MSEgit Copyright (c) 2011 by Martin Schreiber
+   
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+}
+//
+// under construction
+//
+unit commitdiffform;
+{$ifdef FPC}{$mode objfpc}{$h+}{$endif}
+interface
+uses
+ mseglob,mseguiglob,mseguiintf,mseapplication,msestat,msemenus,msegui,
+ msegraphics,msegraphutils,mseevent,mseclasses,mseforms,diffform,msestatfile;
+
+type
+ tcommitdifffo = class(tdifffo)
+   tstatfile1: tstatfile;
+ end;
+var
+ commitdifffo: tcommitdifffo;
+implementation
+uses
+ commitdiffform_mfm;
+end.
