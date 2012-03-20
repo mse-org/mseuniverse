92f4d7b9c24502f644b18351d8c7f92f898c7ce1
Martin Schreiber <mse00000@gmail.com>
2012-03-20 11:34:30 +0100

    * MSEgit: debug apptype console.

--- tools/msegit/msegit.pas
+++ tools/msegit/msegit.pas
@@ -17,7 +17,13 @@
 program msegit;
 {$ifdef FPC}{$mode objfpc}{$h+}{$endif}
 {$ifdef FPC}
- {$ifdef mswindows}{$apptype gui}{$endif}
+ {$ifdef mswindows}
+  {$ifdef mse_debug}
+   {$apptype console}
+  {$else}
+   {$apptype gui}
+  {$endif}
+ {$endif}
 {$endif}
 uses
  {$ifdef FPC}{$ifdef linux}cthreads,{$endif}{$endif}
