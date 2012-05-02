6e4211fd99223210e9d16d3c8cab31ab09e56662
Martin Schreiber <mse00000@gmail.com>
2012-05-02 17:11:17 +0200

    * MSEgit: V1.4a.

--- tools/msegit/main.pas
+++ tools/msegit/main.pas
@@ -25,8 +25,8 @@ uses
  msetimer,mserichstring,msesimplewidgets,msewidgets,msegrids,mseifigui,
  mseifilink;
 const
- versiontext = '1.5 unstable';
-// versiontext = '1.4';
+// versiontext = '1.5 unstable';
+ versiontext = '1.4a';
 type
  tmainfo = class(tmainform)
    dockpanel: tdockpanel;
