4233e9b0331049eaf395284270e8ce8558eb8ff9
Martin Schreiber <mse00000@gmail.com>
2012-11-21 11:11:47 +0100

    + MSEspice file change notify.

--- tools/msespice/netlistform.pas
+++ tools/msespice/netlistform.pas
@@ -21,10 +21,15 @@ type
    procedure saveupdateexe(const sender: tcustomaction);
    procedure saveexe(const sender: TObject);
    procedure saveasexe(const sender: TObject);
+   procedure filechangeexe(const sender: tfilechangenotifyer;
+                   const info: filechangeinfoty);
+   procedure idelexe(var again: Boolean);
   private
    fmodified: boolean;
+   fcheckfilechanged: boolean;
   protected
    procedure updatecaption;
+   function checkfilechanged: boolean;
   public
    procedure loadfile(const afile: filenamety);
    function checksave: boolean; //true if ok
@@ -35,14 +40,18 @@ var
  netlistfo: tnetlistfo;
 implementation
 uses
- netlistform_mfm,msefileutils,msewidgets,mainmodule;
+ netlistform_mfm,msefileutils,msewidgets,mainmodule,msestream,mserichstring;
 
 { tnetlistfo }
 
 procedure tnetlistfo.loadfile(const afile: filenamety);
 begin
  try
+  if edit.filename <> '' then begin
+   filechange.removenotification(edit.filename);
+  end;
   edit.loadfromfile(afile);
+  filechange.addnotification(edit.filename);
  except
   application.handleexception;
  end;
@@ -81,6 +90,7 @@ end;
 
 procedure tnetlistfo.close;
 begin
+ filechange.removenotification(edit.filename);
  edit.clear;
  fmodified:= false;
  updatecaption;
@@ -120,9 +130,73 @@ end;
 procedure tnetlistfo.saveasexe(const sender: TObject);
 begin
  if filedialog.execute(fdk_save) = mr_ok then begin
+  filechange.removenotification(edit.filename);
   edit.savetofile(filedialog.controller.filename);
+  filechange.addnotification(edit.filename);
   mainmo.projectoptions.netlist:= filedialog.controller.filename;
   updatecaption;
+ end;
+end;
+
+function tnetlistfo.checkfilechanged: boolean;
+var
+ stream1: ttextstream;
+ int1,int2: integer;
+ po1: prichstringty;
+ mstr1: msestring;
+begin
+ result:= edit.modified;
+ if not result then begin
+  result:= true;
+  if ttextstream.trycreate(stream1,edit.filename,fm_read) then begin
+                            //else locked or deleted
+   try
+    stream1.encoding:= edit.encoding;
+    int1:= 0;
+    int2:= edit.datalist.count - 1;
+    po1:= edit.datalist.datapo;
+    for int1:= 0 to int2 do begin
+     if not stream1.readln(mstr1) then begin
+      if int1 <> int2 then begin
+       exit;
+      end;
+     end;
+     if mstr1 <> po1^.text then begin
+      exit;
+     end;
+     inc(po1);
+    end;
+    if stream1.eof then begin
+     result:= false;
+    end;
+   finally
+    stream1.free;
+   end;
+  end;
+ end;
+end;
+
+procedure tnetlistfo.filechangeexe(const sender: tfilechangenotifyer;
+               const info: filechangeinfoty);
+begin
+ fcheckfilechanged:= true;
+ application.wakeupmainthread;
+end;
+
+procedure tnetlistfo.idelexe(var again: Boolean);
+var
+ pt1: gridcoordty;
+ bo1:boolean;
+begin
+ if fcheckfilechanged and application.active then begin
+  fcheckfilechanged:= false;
+  bo1:= not edit.modified or askyesno('File "'+edit.filename+
+  '" has changed, there are changes in edit buffer also.'+lineend+'Reload?');
+  if bo1 then begin
+   pt1:= edit.editpos;
+   loadfile(edit.filename);
+   edit.editpos:= pt1;
+  end;   
  end;
 end;
 
