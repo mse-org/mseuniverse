0c03c8414912f901fff61bc410f1694795bc1b25
Martin Schreiber <mse00000@gmail.com>
2013-02-04 12:06:00 +0100

    * Listview demo.

--- attic/msedocumenting/mse/trunk/help/tutorials/widgets/listview/main.pas
+++ attic/msedocumenting/mse/trunk/help/tutorials/widgets/listview/main.pas
@@ -36,14 +36,15 @@ begin
  bmp1:= tmaskedbitmap.create(false);
  application.beginwait;
  try
-  flist1.adddirectory(avalue,fil_name,graphicfilemasks);
+  flist1.adddirectory(avalue,fil_name,graphicfilemasks,[fa_all],[],
+                                                      [dso_caseinsensitive]);
   listview.clear;
   listview.itemlist.count:= flist1.count;  
   imagelist.clear;
   imagelist.count:= flist1.count;
-  for int1:= 0 to imagelist.count - 1 do begin
+  for int1:= 0 to flist1.count - 1 do begin
    bmp1.loadfromfile(avalue+flist1[int1].name);
-   imagelist.setimage(int1,bmp1,true);
+   imagelist.setimage(int1,bmp1,[al_fit,al_intpol,al_xcentered,al_ycentered]);
    listview[int1].imagenr:= int1;
   end;
  finally
