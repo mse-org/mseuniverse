5b5cd9794bba332cb413c49d3f79f8c7c6b0ae26
Martin Schreiber <mse00000@gmail.com>
2013-08-25 09:12:32 +0200

    * lbdropdownfilter demo onbeforefilter.

--- attic/msedocumenting/mse/trunk/help/tutorials/db/lbdropdownfilter/main.pas
+++ attic/msedocumenting/mse/trunk/help/tutorials/db/lbdropdownfilter/main.pas
@@ -2,7 +2,7 @@ unit main;
 {$ifdef FPC}{$mode objfpc}{$h+}{$endif}
 interface
 uses
- mseglob,mseguiglob,mseguiintf,mseapplication,msestat,msemenus,msegui,
+ mclasses,mseglob,mseguiglob,mseguiintf,mseapplication,msestat,msemenus,msegui,
  msegraphics,msegraphutils,mseevent,mseclasses,mseforms,mdb,msebufdataset,
  msedatabase,msedb,mseifiglob,msesqldb,msqldb,sysutils,mselocaldataset,
  mselookupbuffer,msedataedits,msedbedit,mseedit,msegraphedits,msegrids,
@@ -19,22 +19,50 @@ type
    tdbnavigator1: tdbnavigator;
    dropdownedit: tdropdownlisteditlb;
    tstatfile1: tstatfile;
+   caseinsed: tbooleanedit;
+   wordstarted: tbooleaneditradio;
+   wholeworded: tbooleaneditradio;
    procedure filterexe(const sender: tcustomlookupbuffer;
                    const physindex: Integer; var valid: Boolean);
+   procedure beforefilterexe(const sender: tcustomdataedit);
+   procedure searchoptionenteredexe(const sender: TObject);
+  private
+   ffilter: msestring;
+   ffilterupper: msestring;
+   fsearchoptions: searchoptionsty;
  end;
+ 
 var
  mainfo: tmainfo;
+ 
 implementation
 uses
  main_mfm;
- 
+
+procedure tmainfo.beforefilterexe(const sender: tcustomdataedit);
+begin
+ if so_caseinsensitive in fsearchoptions then begin
+  ffilter:= mselowercase(dropdownedit.text);
+  ffilterupper:= mseuppercase(dropdownedit.text);
+ end
+ else begin
+  ffilter:= dropdownedit.text;
+  ffilterupper:= '';
+ end;
+end;
+
 procedure tmainfo.filterexe(const sender: tcustomlookupbuffer;
                const physindex: Integer; var valid: Boolean);
 begin
- if dropdownedit.text <> '' then begin
-  valid:= msestringsearch(dropdownedit.text,
-                   sender.textvalue[1,physindex],1,[so_wordstart]) > 0;
+ if ffilter <> '' then begin
+  valid:= msestringsearch(ffilter,
+         sender.textvalue[1,physindex],1,fsearchoptions,ffilterupper) > 0;
  end;
+end;
+
+procedure tmainfo.searchoptionenteredexe(const sender: TObject);
+begin
+ fsearchoptions:= searchoptionsty(tbooleanedit(sender).valuebitmask);
 end;
 
 end.
