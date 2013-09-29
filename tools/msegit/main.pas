1fb562fecc0327ca3a0a2ba485768752ec4456b5
Martin Schreiber <mse00000@gmail.com>
2013-09-29 12:51:30 +0200

    * MSEgit 1.6.8.

--- tools/msegit/main.pas
+++ tools/msegit/main.pas
@@ -25,7 +25,7 @@ uses
  msetimer,mserichstring,msesimplewidgets,msewidgets,msegrids,mseifigui,
  mseifilink;
 const
- versiontext = '1.6.7';
+ versiontext = '1.6.8';
 type
  tmainfo = class(tmainform)
    dockpanel: tdockpanel;
