9d03237ece5a00449a5b9665072fd3fabb5642fe
Martin Schreiber <mse00000@gmail.com>
2013-10-12 10:09:25 +0200

    + portsettingsframe.

--- samples/communication/commchannel/main.pas
+++ samples/communication/commchannel/main.pas
@@ -6,32 +6,25 @@ uses
  msegraphics,msegraphutils,mseevent,mseclasses,mseforms,msecommport,
  msecommutils,msedataedits,mseedit,mseificomp,mseificompglob,mseifiglob,
  msestrings,msetypes,msegraphedits,msescrollbar,msegrids,msesimplewidgets,
- msewidgets,msestatfile,msesercomm,msedispwidgets,mserichstring;
+ msewidgets,msestatfile,msesercomm,msedispwidgets,mserichstring,
+ portsettingsframe;
 
 type
  tmainfo = class(tmainform)
-   ported: tcommselector;
-   activeed: tbooleanedit;
    senddtexted: thistoryedit;
    rxdata: tstringgrid;
    clearbu: tbutton;
    tstatfile1: tstatfile;
-   port: tsercommcomp;
-   eor: thexstringedit;
    conn: tasynsercommchannel;
    statusdisp: tstringdisp;
+   portfra: tportsettingsfra;
    procedure clearexe(const sender: TObject);
-   procedure portsetexe(const sender: TObject; var avalue: commnrty;
-                   var accept: Boolean);
-   procedure setactiveexe(const sender: TObject; var avalue: Boolean;
-                   var accept: Boolean);
    procedure sendtextexe(const sender: TObject; var avalue: msestring;
                    var accept: Boolean);
    procedure responseexe(const sender: tcustomsercommchannel;
                    var adata: AnsiString; var aflags: commresponseflagsty);
    procedure seteorexe(const sender: TObject; var avalue: AnsiString;
                    var accept: Boolean);
-   procedure getactivecomexe(const sender: TObject; var avalue: commnrty);
  end;
 var
  mainfo: tmainfo;
@@ -44,39 +37,6 @@ uses
 procedure tmainfo.clearexe(const sender: TObject);
 begin
  rxdata.clear;
-end;
-
-procedure tmainfo.portsetexe(const sender: TObject; var avalue: commnrty;
-               var accept: Boolean);
-begin
- try
-  if avalue = cnr_invalid then begin 
-   port.port.commname:= tcommselector(sender).valuename; //special comm name
-  end
-  else begin
-   port.port.commname:= '';         
-   port.port.commnr:= avalue;               //normal comm number
-  end;
- finally
-  activeed.value:= port.active; //reset in case of error
- end;
-end;
-
-procedure tmainfo.getactivecomexe(const sender: TObject; var avalue: commnrty);
-begin
- //add current port to dropdownlist
- if port.active and (port.port.commname = '') then begin
-  avalue:= port.port.commnr;
- end;
-end;
-
-procedure tmainfo.setactiveexe(const sender: TObject; var avalue: Boolean;
-               var accept: Boolean);
-begin
- conn.connected:= avalue;
- if not avalue then begin
-  port.active:= false;
- end;
 end;
 
 procedure tmainfo.sendtextexe(const sender: TObject; var avalue: msestring;
--- samples/communication/r232direct/main.pas
+++ samples/communication/r232direct/main.pas
@@ -6,27 +6,19 @@ uses
  msegraphics,msegraphutils,mseevent,mseclasses,mseforms,msecommport,
  msecommutils,msedataedits,mseedit,mseificomp,mseificompglob,mseifiglob,
  msestrings,msetypes,msegraphedits,msescrollbar,msegrids,msesimplewidgets,
- msewidgets,msestatfile,msesercomm,msetimer;
+ msewidgets,msestatfile,msesercomm,msetimer,portsettingsframe;
 
 type
  tmainfo = class(tmainform)
-   ported: tcommselector;
-   activeed: tbooleanedit;
    senddtexted: thistoryedit;
    rxdata: tstringgrid;
    clearbu: tbutton;
    tstatfile1: tstatfile;
-   port: tsercommcomp;
-   eor: thexstringedit;
    ttimer1: ttimer;
+   portfra: tportsettingsfra;
    procedure clearexe(const sender: TObject);
-   procedure portsetexe(const sender: TObject; var avalue: commnrty;
-                   var accept: Boolean);
-   procedure setactiveexe(const sender: TObject; var avalue: Boolean;
-                   var accept: Boolean);
    procedure sendtextexe(const sender: TObject; var avalue: msestring;
                    var accept: Boolean);
-   procedure getactivecommexe(const sender: TObject; var avalue: commnrty);
    procedure timerexe(const sender: TObject);
  end;
 var
@@ -37,48 +29,18 @@ implementation
 uses
  main_mfm,sysutils;
 
-procedure tmainfo.portsetexe(const sender: TObject; var avalue: commnrty;
-               var accept: Boolean);
-begin
- try
-  if avalue = cnr_invalid then begin 
-   port.port.commname:= tcommselector(sender).valuename; //special comm name
-  end
-  else begin
-   port.port.commname:= '';         
-   port.port.commnr:= avalue;               //normal comm number
-  end;
- finally
-  activeed.value:= port.active; //reset in case of error
- end;
-end;
-
-procedure tmainfo.getactivecommexe(const sender: TObject; var avalue: commnrty);
- //add current port to dropdown list
-begin
- if port.active and (port.port.commname = '') then begin
-  avalue:= port.port.commnr;
- end;
-end;
-
-procedure tmainfo.setactiveexe(const sender: TObject; var avalue: Boolean;
-               var accept: Boolean);
-begin
- port.active:= avalue;
-end;
-
 procedure tmainfo.sendtextexe(const sender: TObject; var avalue: msestring;
                var accept: Boolean);
 begin
- port.port.writestring(avalue+eor.value);
+ portfra.port.port.writestring(avalue+portfra.eor.value);
 end;
 
 procedure tmainfo.timerexe(const sender: TObject);
 var
  str1: string;
 begin
- if port.active then begin
-  str1:= port.port.readavailstring(100); //max 100 byte
+ if portfra.port.active then begin
+  str1:= portfra.port.port.readavailstring(100); //max 100 byte
   if str1 <> '' then begin
    rxdata[0].readpipe(str1);   
    rxdata.showrow(bigint);
