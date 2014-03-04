409ac0e36934c4c2bc00eb51bf13440be148db29
Martin Schreiber <mse00000@gmail.com>
2014-03-04 11:14:45 +0100

    * Grayscale testcase.

--- testcase/graphics/grayscale/main.pas
+++ testcase/graphics/grayscale/main.pas
@@ -4,12 +4,19 @@ interface
 uses
  msetypes,mseglob,mseguiglob,mseguiintf,mseapplication,msestat,msemenus,msegui,
  msegraphics,msegraphutils,mseevent,mseclasses,msewidgets,mseforms,msedataedits,
- mseedit,mseificomp,mseificompglob,mseifiglob,msestrings,msesimplewidgets;
+ mseedit,mseificomp,mseificompglob,mseifiglob,msestrings,msesimplewidgets,
+ mseimage,msestatfile;
 
 type
  tmainfo = class(tmainform)
    tbutton1: tbutton;
+   di: timage;
+   tenumtypeedit1: tenumtypeedit;
+   tstatfile1: tstatfile;
    procedure exe(const sender: TObject);
+   procedure kindsetexe(const sender: TObject; var avalue: Integer;
+                   var accept: Boolean);
+   procedure kindinitexe(const sender: tenumtypeedit);
  end;
 var
  mainfo: tmainfo;
@@ -22,6 +29,18 @@ var
  ha1: pixmapty;
 begin
  ha1:= gui_createpixmap(ms(100,50),0,bmk_gray);
+ gui_freepixmap(ha1);
+end;
+
+procedure tmainfo.kindsetexe(const sender: TObject; var avalue: Integer;
+               var accept: Boolean);
+begin
+ di.bitmap.kind:= bitmapkindty(avalue);
+end;
+
+procedure tmainfo.kindinitexe(const sender: tenumtypeedit);
+begin
+ sender.typeinfopo:= typeinfo(bitmapkindty);
 end;
 
 end.
