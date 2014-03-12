7ba6d8535e611cc943c2a9722aa05fed6f356aa8
Martin Schreiber <mse00000@gmail.com>
2014-03-12 10:22:28 +0100

    + Bitmap testcase.

---
+++ testcase/graphics/bitmap/main.pas
@@ -0,0 +1,229 @@
+unit main;
+{$ifdef FPC}{$mode objfpc}{$h+}{$INTERFACES CORBA}{$endif}
+interface
+uses
+ mseforms,msecalendardatetimeedit,msedataedits,mseedit,mseglob,msegui,
+ mseguiglob,mseifiglob,msemenus,msestrings,msetypes,msedispwidgets,
+ msegraphics,msegraphutils,msesimplewidgets,msewidgets,
+ msecolordialog,msestat,msestatfile,msegraphedits,mseclasses,msegrids,
+ msewidgetgrid,mseterminal,msethreadcomp,sysutils,
+ mseapplication,
+ mseimage,mseificompglob,msebitmap,
+ msefiledialog; 
+type
+ tmainfo = class(tdockform)
+   ima: timage;
+   graymasked: tbooleanedit;
+   colormasked: tbooleanedit;
+   kinded: tenumtypeedit;
+   maskeded: tbooleanedit;
+   fileed: tfilenameedit;
+   tbutton1: tbutton;
+   tstatfile1: tstatfile;
+   bmpfged: tcoloredit;
+   bmpbged: tcoloredit;
+   fged: tcoloredit;
+   bged: tcoloredit;
+   tbutton3: tbutton;
+   mbged: tcoloredit;
+   mfged: tcoloredit;
+   tbutton4: tbutton;
+   bmptransed: tcoloredit;
+   stretchxed: tbooleanedit;
+   stretchyed: tbooleanedit;
+   intpoled: tbooleanedit;
+   opaed: tcoloredit;
+   destkinded: tenumtypeedit;
+   dima: timage;
+   procedure loadedexe(const sender: TObject);
+   procedure initexe(const sender: tenumtypeedit);
+   procedure graymaskset(const sender: TObject; var avalue: Boolean;
+                   var accept: Boolean);
+   procedure colormaskset(const sender: TObject; var avalue: Boolean;
+                   var accept: Boolean);
+   procedure kindset(const sender: TObject; var avalue: Integer;
+                   var accept: Boolean);
+   procedure maskedset(const sender: TObject; var avalue: Boolean;
+                   var accept: Boolean);
+   procedure loadexe(const sender: TObject);
+   procedure datentexe(const sender: TObject);
+   procedure setbmpbg(const sender: TObject; var avalue: colorty;
+                   var accept: Boolean);
+   procedure setbmpfg(const sender: TObject; var avalue: colorty;
+                   var accept: Boolean);
+   procedure paintexe(const sender: TObject);
+   procedure maskpaintexe(const sender: TObject);
+   procedure setbmptrans(const sender: TObject; var avalue: colorty;
+                   var accept: Boolean);
+   procedure setstretchy(const sender: TObject; var avalue: Boolean;
+                   var accept: Boolean);
+   procedure setstretchx(const sender: TObject; var avalue: Boolean;
+                   var accept: Boolean);
+   procedure setintpol(const sender: TObject; var avalue: Boolean;
+                   var accept: Boolean);
+   procedure setopa(const sender: TObject; var avalue: colorty;
+                   var accept: Boolean);
+   procedure dkindset(const sender: TObject; var avalue: Integer;
+                   var accept: Boolean);
+   procedure imapaexe(const sender: twidget; const acanvas: tcanvas);
+  private
+   procedure check();
+ end;
+var
+ mainfo: tmainfo;
+
+implementation
+uses
+ main_mfm,mseformatstr,msemime,msedatalist,mseprocutils,msefileutils,
+ mseformatpngread,windows,mseguiintf;
+
+procedure tmainfo.check;
+begin
+ with ima.bitmap do begin
+  kinded.value:= ord(kind);
+  maskeded.value:= masked;
+  graymasked.value:= graymask;
+  colormasked.value:= colormask;
+  bmpfged.value:= colorforeground;
+  bmpbged.value:= colorbackground;
+  bmptransed.value:= transparentcolor;
+ end;
+end;
+ 
+procedure tmainfo.loadedexe(const sender: TObject);
+begin
+ dima.bitmap.size:= dima.paintsize;
+ check();
+end;
+
+procedure tmainfo.initexe(const sender: tenumtypeedit);
+begin
+ sender.typeinfopo:= typeinfo(bitmapkindty);
+end;
+
+procedure tmainfo.graymaskset(const sender: TObject; var avalue: Boolean;
+               var accept: Boolean);
+begin
+ ima.bitmap.graymask:= avalue;
+end;
+
+procedure tmainfo.colormaskset(const sender: TObject; var avalue: Boolean;
+               var accept: Boolean);
+begin
+ ima.bitmap.colormask:= avalue;
+end;
+
+procedure tmainfo.kindset(const sender: TObject; var avalue: Integer;
+               var accept: Boolean);
+begin
+ ima.bitmap.kind:= bitmapkindty(avalue);
+end;
+
+procedure tmainfo.maskedset(const sender: TObject; var avalue: Boolean;
+               var accept: Boolean);
+begin
+ ima.bitmap.masked:= avalue;
+end;
+
+procedure tmainfo.loadexe(const sender: TObject);
+begin
+ ima.bitmap.loadfromfile(fileed.value,'',[]);
+ check();
+end;
+
+procedure tmainfo.datentexe(const sender: TObject);
+begin
+ check;
+ ima.invalidate();
+end;
+
+procedure tmainfo.setbmpbg(const sender: TObject; var avalue: colorty;
+               var accept: Boolean);
+begin
+ ima.bitmap.colorbackground:= avalue;
+end;
+
+procedure tmainfo.setbmpfg(const sender: TObject; var avalue: colorty;
+               var accept: Boolean);
+begin
+ ima.bitmap.colorforeground:= avalue;
+end;
+
+procedure tmainfo.setbmptrans(const sender: TObject; var avalue: colorty;
+               var accept: Boolean);
+begin
+ ima.bitmap.transparentcolor:= avalue;
+end;
+
+procedure tmainfo.paintexe(const sender: TObject);
+begin
+ with ima.bitmap do begin
+  init(bged.value);
+  canvas.fillellipse1(mr(nullpoint,size),fged.value);
+  canvas.drawline(nullpoint,pointty(size),fged.value);
+ end;
+end;
+
+procedure tmainfo.maskpaintexe(const sender: TObject);
+begin
+ if ima.bitmap.masked then begin
+  with ima.bitmap.mask do begin
+   init(mbged.value);
+   canvas.fillellipse1(mr(nullpoint,size),mfged.value);
+   canvas.drawline(nullpoint,pointty(size),mfged.value);
+  end;
+ end;
+end;
+
+procedure tmainfo.setstretchy(const sender: TObject; var avalue: Boolean;
+               var accept: Boolean);
+begin
+ if avalue then begin
+  ima.bitmap.alignment:= ima.bitmap.alignment + [al_stretchy];
+ end
+ else begin
+  ima.bitmap.alignment:= ima.bitmap.alignment - [al_stretchy];
+ end;
+end;
+
+procedure tmainfo.setstretchx(const sender: TObject; var avalue: Boolean;
+               var accept: Boolean);
+begin
+ if avalue then begin
+  ima.bitmap.alignment:= ima.bitmap.alignment + [al_stretchx];
+ end
+ else begin
+  ima.bitmap.alignment:= ima.bitmap.alignment - [al_stretchx];
+ end;
+end;
+
+procedure tmainfo.setintpol(const sender: TObject; var avalue: Boolean;
+               var accept: Boolean);
+begin
+ if avalue then begin
+  ima.bitmap.alignment:= ima.bitmap.alignment + [al_intpol];
+ end
+ else begin
+  ima.bitmap.alignment:= ima.bitmap.alignment - [al_intpol];
+ end;
+end;
+
+procedure tmainfo.setopa(const sender: TObject; var avalue: colorty;
+               var accept: Boolean);
+begin
+ ima.bitmap.opacity:= avalue;
+end;
+
+procedure tmainfo.dkindset(const sender: TObject; var avalue: Integer;
+               var accept: Boolean);
+begin
+ dima.bitmap.kind:= bitmapkindty(avalue);
+end;
+
+procedure tmainfo.imapaexe(const sender: twidget; const acanvas: tcanvas);
+begin
+ dima.bitmap.init(cl_white);
+ ima.bitmap.paint(dima.bitmap.canvas,mr(nullpoint,dima.bitmap.size));
+end;
+
+end.
