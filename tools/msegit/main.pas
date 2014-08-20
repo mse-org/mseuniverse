c5b9c47bcef52c8656d80d8deca69f9422182c2c
Martin Schreiber <mse00000@gmail.com>
2014-08-20 08:22:55 +0200

    + MSEgit 2.0a. + MSEspice 1.4a.

--- tools/msegit/main.pas
+++ tools/msegit/main.pas
@@ -25,7 +25,7 @@ uses
  msetimer,mserichstring,msesimplewidgets,msewidgets,msegrids,mseifigui,
  mseifilink,finddialogform;
 const
- versiontext = '2.0';
+ versiontext = '2.0a';
 type
  tmainfo = class(tmainform)
    dockpanel: tdockpanel;
