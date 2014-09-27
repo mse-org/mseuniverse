eb2c6b5291f044fb1d0b9d3c93c32bde3ac44fcb
Martin Schreiber <mse00000@gmail.com>
2014-09-27 12:15:48 +0200

    + Sorting benchmark.

---
+++ testcase/benchmark/sorting/fphashlistsort.pas
@@ -0,0 +1,166 @@
+unit FPHashListSort;
+
+{ $mode objfpc}{$H-}
+
+interface
+uses
+  Classes, SysUtils, Contnrs;
+
+type
+  TSortArray   = array of PShortString;
+
+  TCompareFunc = function(const S1, S2: ShortString): SmallInt;
+{ TCompareFunc = function(const S1, S2: String): Integer;  // alternativ für CompareStr/Text }
+
+function CompareShortStringAnsiCharSensitive(const S1, S2 : ShortString): SmallInt; //inline;
+function CompareShortStringAnsiCharInsensitive(const S1, S2 : ShortString) : SmallInt; //inline;
+procedure QuickSortHashList({const HL: TFPHashList; }var SA : TSortArray; Compare: TCompareFunc);
+
+implementation
+
+{   Results:  S1 < S2  < 0
+              S1 > S2  > 0                (proved, okay)
+              S1 = S2  = 0     }
+
+
+{function CompareShortStringAnsiCharSensitive(const S1, S2 : ShortString): SmallInt;
+var                                         // simple, but too slow
+  i, len : SmallInt;
+begin
+  if S1[0] > S2[0] then len := Byte(S2[0]) else len := Byte(S1[0]);
+  i := 1;
+  while (i < len) and (S1[i] = S2[i]) do inc(i);
+  result := Byte(S1[i]) - Byte(S2[i]);      // now either i=len or S1[i]<>S2[i]
+  if result = 0 then result := Byte(S1[0]) - Byte(S2[0]);
+end;}
+
+{$GOTO+}
+
+function CompareShortStringAnsiCharSensitive(const S1, S2 : ShortString): SmallInt;
+var
+  i, len : SmallInt;                     // SmallInt are faster than Byte
+label L1;
+begin
+  if Length(S1) > Length(S2) then len := Length(S2) else len := Length(S1);
+  i := 1;                                // Length(S1) is coded same as Byte(S1[0])
+  while i <= len do
+  begin
+    if (S1[i] = S2[i]) then inc(i)
+    else begin
+      result := Byte(S1[i]) - Byte(S2[i]);
+      goto L1;                           // Label is faster than Break
+    end;
+  end;
+  result := Length(S1) - Length(S2);     // only reached if all matched and i > len
+  L1:
+end;
+
+function CompareShortStringAnsiCharInsensitive(const S1, S2 : ShortString) : SmallInt;
+var
+  i, len, B1, B2 : Byte;           // all Byte are faster than i & len as SmallInt
+label L2;
+begin
+  if Length(S1) > Length(S2) then len := Length(S2) else len := Length(S1);
+  i := 1;                          // Length(S1) is as fast as Byte(S1[0])
+  while i <= len do
+  begin
+    B1 := Byte(S1[i]);             // must have copies to modify, because S1/S2 are consts
+    B2 := Byte(S2[i]);             // doing it here accelerates first-char-mismatch and
+    if B1 = B2 then inc(i)         // different case matches
+    else begin
+      if B1 in [97..122] then dec(B1, 32);
+      if B2 in [97..122] then dec(B2, 32);
+      if B1 = B2 then inc(i)       // sensitivity match
+      else begin
+        result := B1 - B2;         // mismatch inside of the strings
+        goto L2;                   // Label is faster than Break
+      end;
+    end;
+  end;
+  result := Length(S1) - Length(S2);   // only reached if all matched and i > len
+  L2:
+end;
+
+{function CompareShortStringAnsiCharInsensitive(const S1, S2 : ShortString) : SmallInt;
+var                                 // fast on the long run, but slow at early breaks
+  B1, B2 : Byte;                    // okay: not better than above even on the long run
+  P1, P2, P1Last : PByte;
+begin
+  P1 := @S1;
+  P2 := @S2;
+  if P1^ > P2^ then P1Last := P1 + P2^ else P1Last := P1 + P1^;
+  result := 0;
+  inc(P1);
+  inc(P2);
+  while P1 < P1Last do
+  begin
+    if P1^ = P2^ then begin inc(P1); inc(P2); end
+    else begin
+      B1 := P1^;
+      B2 := P2^;
+      if B1 in [97..122] then dec(B1, 32);
+      if B2 in [97..122] then dec(B2, 32);
+      if B1 = B2 then begin inc(P1); inc(P2); end
+      else begin
+        result := B1 - B2;
+        break;
+      end;
+    end;
+  end;
+  if result = 0 then result := Byte(S1[0]) - Byte(S2[0]);
+end;}
+
+procedure QuickSortHashList({const HL: TFPHashList;} var SA : TSortArray; Compare: TCompareFunc);
+//var
+//  n : integer;
+
+  {Sub-}
+  procedure DoQuickSort(L, R: integer);
+  var
+    I, J, P        : Integer;
+    tmp, PivotItem : PShortString;
+  begin
+    repeat
+      I := L;
+      J := R;
+      P := (L + R) div 2;
+      repeat
+        PivotItem := SA[P];
+        while Compare(PivotItem^, SA[I]^) > 0 do Inc(I);
+        while Compare(PivotItem^, SA[J]^) < 0 do Dec(J);
+        if I <= J then
+        begin
+          tmp := SA[i];
+          SA[I] := SA[J];
+          SA[J] := tmp;
+          if P = I then
+            P := J
+          else if P = J then
+            P := I;
+          Inc(I);
+          Dec(J);
+        end;
+      until I > J;
+      if L < J then
+        DoQuickSort(L, J);
+      L := I;
+    until I >= R;
+  end;
+  {/Subprocedure DoQuickSort}
+
+{main QuickSortHashList}
+begin
+{
+  if not Assigned(HL) or not Assigned (Compare) then exit;
+  SetLength(SA, HL.Count);
+  for n := 0 to HL.Count - 1 do SA[n] := PShortString(@HL.Strs[HL.List^[n].StrIndex]);
+  if (HL.Count < 2) then exit;                  // Anordnung wg. Sonderfall Count=1
+  DoQuickSort(0, HL.Count-1);
+}
+ if length(sa) > 1 then begin
+  doquicksort(0,high(sa));
+ end;
+end;
+
+end.
+
---
+++ testcase/benchmark/sorting/main.pas
@@ -0,0 +1,193 @@
+
+// Linux only because of low now() granularity on Windows!
+
+unit main;
+{$ifdef FPC}{$mode delphi}{$h+}{$endif}
+interface
+uses
+ msegui,mseforms,mseguiglob,msewidgets,mseglob,msestat,msestatfile,
+ msestrings,msetypes,msedispwidgets,msegrids,msegraphedits,msesimplewidgets,
+ msedataedits;
+ 
+type
+ tmainfo = class(tmseform)
+   grid: tstringgrid;
+   tstatfile1: tstatfile;
+   tbutton2: tbutton;
+   sodi: trealdisp;
+   sosumdi: trealdisp;
+   sumdi: trealdisp;
+   ndi: tintegerdisp;
+   modeed: tenumtypeedit;
+   di: trealdisp;
+   tbutton1: tbutton;
+   fpsodi: trealdisp;
+   fpsosumdi: trealdisp;
+   fpsumdi: trealdisp;
+   fpdi: trealdisp;
+   ned: tintegeredit;
+   tbutton3: tbutton;
+   pcharsumdi: trealdisp;
+   sopcharsumdi: trealdisp;
+   sopchardi: trealdisp;
+   pchardi: trealdisp;
+   procedure runexe(const sender: TObject);
+   procedure initexe(const sender: tenumtypeedit);
+   procedure resetexe(const sender: TObject);
+   procedure datentexe(const sender: TObject);
+   procedure copyexe(const sender: TObject);
+  private
+ end;
+
+var
+ mainfo: tmainfo;
+
+implementation
+uses
+ main_mfm,msenoise,msearrayutils,msedate,fphashlistsort;
+
+procedure tmainfo.runexe(const sender: TObject);
+const
+ strlen = $20;
+var
+ ti1: tdatetime;
+ ar1: stringarty;
+ ar2: msestringarty;
+ ar3: array of string[strlen+1];
+ ar4,ar5: tsortarray;
+ int1,int2,int3: integer;
+ po1: pchar;
+ ch1: char;
+ compfunc: tcomparefunc;
+ arlen: integer;
+begin
+ arlen:= ned.value;
+ setlength(ar1,arlen);
+ for int1:= 0 to high(ar1) do begin
+  int3:= mwcnoise and (strlen-1);
+  setlength(ar1[int1],int3);
+  po1:= pointer(ar1[int1]);
+  for int2:= 0 to int3-1 do begin
+   repeat
+    ch1:= char(mwcnoise() and $7f);
+   until (ch1 >= #32) and (ch1 < #127);
+   po1[int2]:= ch1;
+  end;
+ end;
+ setlength(ar3,arlen);
+ setlength(ar4,arlen);
+ for int1:= 0 to high(ar3) do begin
+  ar3[int1]:= ar1[int1];
+  ar3[int1][length(ar3[int1])+1]:= #0;
+  ar4[int1]:= @ar3[int1];
+ end;
+ ar5:= copy(ar4);
+ for int1:= 0 to high(ar5) do begin
+  inc(pchar(pointer(ar5[int1])));
+ end;
+ ti1:= nowutc;                                        //MSEgui ansistring first
+ sortarray(ar1,stringsortmodety(modeed.value));
+ ti1:= (nowutc-ti1)*3600*24;
+ di.value:= ti1;
+ sumdi.value:= sumdi.value*ndi.value+ti1;
+ ndi.value:= ndi.value+1;
+ sumdi.value:= sumdi.value/ndi.value;
+
+ ti1:= nowutc;                                        //MSEgui ansistring second
+ sortarray(ar1,stringsortmodety(modeed.value));
+ ti1:= (nowutc-ti1)*3600*24;
+ sodi.value:= ti1;
+ sosumdi.value:= sosumdi.value*(ndi.value-1)+ti1;
+ sosumdi.value:= sosumdi.value/ndi.value;
+
+ setlength(ar2,length(ar1));
+ for int1:= 0 to high(ar1) do begin
+  ar2[int1]:= ar1[int1];
+ end;
+ grid[0].datalist.asarray:= ar2;
+                                                    
+ ti1:= nowutc;                                        //MSEgui pchar first 
+ sortarray(stringarty(ar5),stringsortmodety(modeed.value));
+ ti1:= (nowutc-ti1)*3600*24;
+ pchardi.value:= ti1;
+ pcharsumdi.value:= pcharsumdi.value*(ndi.value-1)+ti1;
+ pcharsumdi.value:= pcharsumdi.value/ndi.value;
+
+ ti1:= nowutc;                                        //MSEgui pchar second
+ sortarray(stringarty(ar5),stringsortmodety(modeed.value));
+ ti1:= (nowutc-ti1)*3600*24;
+ sopchardi.value:= ti1;
+ sopcharsumdi.value:= sopcharsumdi.value*(ndi.value-1)+ti1;
+ sopcharsumdi.value:= sopcharsumdi.value/ndi.value;
+
+ setlength(ar2,length(ar1));
+ for int1:= 0 to high(ar1) do begin
+  ar2[int1]:= msestring(pchar(pointer(ar5[int1])));
+ end;
+ grid[1].datalist.asarray:= ar2;
+ 
+ case stringsortmodety(modeed.value) of
+  sms_upiascii: begin
+   compfunc:= @CompareShortStringAnsiCharInSensitive;
+  end;
+  else begin
+   compfunc:= @CompareShortStringAnsiCharSensitive;
+  end;
+ end;
+ 
+ ti1:= nowutc;                                        //fp first
+ quicksorthashlist(ar4,compfunc);
+ ti1:= (nowutc-ti1)*3600*24;
+ fpdi.value:= ti1;
+ fpsumdi.value:= fpsumdi.value*(ndi.value-1)+ti1;
+ fpsumdi.value:= fpsumdi.value/ndi.value;
+
+ ti1:= nowutc;                                        //fp second
+ quicksorthashlist(ar4,compfunc);
+ ti1:= (nowutc-ti1)*3600*24;
+ fpsodi.value:= ti1;
+ fpsosumdi.value:= fpsosumdi.value*(ndi.value-1)+ti1;
+ fpsosumdi.value:= fpsosumdi.value/ndi.value;
+
+ setlength(ar2,length(ar4));
+ for int1:= 0 to high(ar4) do begin
+  ar2[int1]:= ar4[int1]^;
+ end;
+ grid[2].datalist.asarray:= ar2;
+ copyexe(nil);
+end;
+
+procedure tmainfo.initexe(const sender: tenumtypeedit);
+begin
+ sender.typeinfopo:= typeinfo(stringsortmodety);
+end;
+
+procedure tmainfo.resetexe(const sender: TObject);
+begin
+ ndi.value:= 0;
+ sumdi.value:= 0;
+ sosumdi.value:= 0;
+ pcharsumdi.value:= 0;
+ sopcharsumdi.value:= 0;
+ fpsumdi.value:= 0;
+ fpsosumdi.value:= 0;
+end;
+
+procedure tmainfo.datentexe(const sender: TObject);
+begin
+ resetexe(nil);
+end;
+
+procedure tmainfo.copyexe(const sender: TObject);
+begin
+ copytoclipboard( 
+'Mode: '+modeed.disptext+' Runs: '+ndi.disptext+lineend+
+'           MSEgui first MSEgui second MSEgui first MSEgui second fp first    fp second'+lineend+
+'           ansistring   ansistring    pchar        pchar         shortstring shortstring'+lineend+
+'N: '+fitstring(ned.disptext,7,sp_right)+' '+
+sumdi.disptext+'    '+sosumdi.disptext+'     '+pcharsumdi.disptext+'    '+sopcharsumdi.disptext+'     '+
+fpsumdi.disptext+' '+fpsosumdi.disptext);
+ 
+end;
+
+end.
