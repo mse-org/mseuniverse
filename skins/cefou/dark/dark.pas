81dd94d0543ac7f87e03769d49793e3eec974d46
Martin Schreiber <mse00000@gmail.com>
2014-12-24 18:04:57 +0100

    + dark skin from cefou.

---
+++ skins/cefou/dark/dark.pas
@@ -0,0 +1,76 @@
+unit dark;
+{$ifdef FPC}{$mode objfpc}{$h+}{$endif}
+interface
+uses
+ msetypes,mseglob,mseapplication,mseclasses,msedatamodules,mseskin,msegui,
+ msegraphics,msegraphutils,mseguiglob,msemenus,msesimplewidgets,msewidgets;
+
+type
+ tdarkmo = class(tmsedatamodule)
+   tskincontroller1: tskincontroller;
+   buttonface: tfacecomp;
+   buttonframe: tframecomp;
+   EditFace: tfacecomp;
+   EditFrame: tframecomp;
+   ContainerFace: tfacecomp;
+   GroupBoxFrame: tframecomp;
+   GroupBoxFace: tfacecomp;
+   DispWidgetFrame: tframecomp;
+   ToolBarFace: tfacecomp;
+   ToolBarFrame: tframecomp;
+   ToolBarButtonFace: tfacecomp;
+   BooleanEditFace: tfacecomp;
+   BooleanEditFrame: tframecomp;
+   GridDataColFace: tfacecomp;
+   GridFace: tfacecomp;
+   GridFixedColFace: tfacecomp;
+   GridFixedRowFace: tfacecomp;
+   GridFrame: tframecomp;
+   GridFixedColFrame: tframecomp;
+   GridFixedRowFrame: tframecomp;
+   GridDataColFrame: tframecomp;
+   SliderFaceButtonHoriz: tfacecomp;
+   SliderFaceEndButtonHoriz: tfacecomp;
+   SliderFrameButtonHoriz: tframecomp;
+   SliderFrameEndButton1Horiz: tframecomp;
+   SliderFrameEndButton2Horiz: tframecomp;
+   SliderFace: tfacecomp;
+   SliderFrame: tframecomp;
+   SliderFaceButtonVertical: tfacecomp;
+   SliderFaceEndButtonVertical: tfacecomp;
+   SliderFrameButtonVertical: tframecomp;
+   SliderFrameEndBUtton1Vertical: tframecomp;
+   SliderFrameEndBUtton2Vertical: tframecomp;
+   MenuFace: tfacecomp;
+   MenuFrame: tframecomp;
+   MenuItemFace: tfacecomp;
+   MenuItemFaceActive: tfacecomp;
+   MenuItemFrame: tframecomp;
+   MenuItemFrameActive: tframecomp;
+   MenuPopupCheckBxFrame: tframecomp;
+   MenuPopupFace: tfacecomp;
+   MenuPopupFrame: tframecomp;
+   MenuPopupItemFace: tfacecomp;
+   MenuPopupItemFaceActive: tfacecomp;
+   MenuPopupItemFrame: tframecomp;
+   MenuPopupItemFrameActive: tframecomp;
+   MenuPopupSeparatorFrame: tframecomp;
+   MenuSeparatorFrame: tframecomp;
+   StepButtonFace: tfacecomp;
+   StepButtonFrame: tframecomp;
+   FrameButton: tframecomp;
+   FaceButton: tfacecomp;
+   TabFace: tfacecomp;
+   TabFrame: tframecomp;
+   TabBarHorizFace: tfacecomp;
+   TabBarHorizFrame: tframecomp;
+   TabBarTabFace: tfacecomp;
+   TabBarTabFaceActive: tfacecomp;
+   TabBarTabFrame: tframecomp;
+ end;
+var
+ darkmo: tdarkmo;
+implementation
+uses
+ dark_mfm;
+end.
---
+++ skins/cefou/dark/darkskinloader.pas
@@ -0,0 +1,9 @@
+unit darkskinLoader;
+{$ifdef FPC}{$mode objfpc}{$h+}{$endif}
+interface
+implementation
+uses
+ msegui,dark;
+initialization
+ application.createdatamodule(tdarkmo,darkmo);
+end.
