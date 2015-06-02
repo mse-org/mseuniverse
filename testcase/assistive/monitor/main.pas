f781a86071685a0ef031e54588c8cf4d9c74bb32
Martin Schreiber <mse00000@gmail.com>
2015-06-02 09:40:49 +0200

    * Updated assistivemonitor.

--- testcase/assistive/monitor/main.pas
+++ testcase/assistive/monitor/main.pas
@@ -5,18 +5,26 @@ uses
  msetypes,mseglob,mseguiglob,mseguiintf,mseapplication,msestat,msemenus,msegui,
  msegraphics,msegraphutils,mseevent,mseclasses,msewidgets,mseforms,
  mseassistive,msegrids,msestrings,msesimplewidgets,msedataedits,mseedit,
- mseificomp,mseificompglob,mseifiglob,msestatfile,msestream,sysutils;
+ mseificomp,mseificompglob,mseifiglob,msestatfile,msestream,sysutils,
+ mseact;
 
 type
  tassistivemonitor = class(tobject,iassistiveserver)
   private
    fgrid: tstringgrid;
   protected
-   procedure track(const sender: iassistiveclient; const atext: msestring);
+   procedure track(const prefix: msestring; const sender: iassistiveclient; 
+                                                   const atext: msestring);
+   procedure track(const prefix: msestring; const sender: tobject; 
+                                                     const atext: msestring);
     //iassistiveserver
    procedure doenter(const sender: iassistiveclient);
    procedure clientmouseevent(const sender: iassistiveclient;
                                            var info: mouseeventinfoty);
+   procedure dofocuschanged(const oldwidget,newwidget: iassistiveclient);
+   procedure dokeydown(const sender: iassistiveclient;
+                                        const info: keyeventinfoty);
+   procedure doactionexecute(const sender: tobject; const info: actioninfoty);
   public
    constructor create(const agrid: tstringgrid);
    destructor destroy(); override;
@@ -30,15 +38,27 @@ type
    tstringedit2: tstringedit;
    procedure createexe(const sender: TObject);
    procedure destroyexe(const sender: TObject);
+   procedure exe(const sender: TObject);
   private
    fmonitor: tassistivemonitor;
  end;
  
 var
  mainfo: tmainfo;
+
 implementation
 uses
- main_mfm,typinfo,mclasses;
+ main_mfm,typinfo,mclasses,mseactions;
+
+function getname(const sender: iassistiveclient): msestring;
+begin
+ if sender = nil then begin
+  result:= 'NIL';
+ end
+ else begin
+  result:= sender.getassistivename;
+ end;
+end;
  
 { tassistivemonitor }
 
@@ -54,31 +74,71 @@ begin
  fgrid.free();
 end;
 
+procedure tassistivemonitor.track(const prefix: msestring; 
+                       const sender: iassistiveclient; const atext: msestring);
+var
+ mstr1: msestring;
+// inst1: tobject;               
+begin
+ if (sender = nil) or (sender.getinstance <> fgrid) then begin
+  mstr1:= '<'+getname(sender)+'> ';
+  fgrid.appendrow(prefix+mstr1+atext);
+  fgrid.showlastrow();
+ end;
+end;
+
+procedure tassistivemonitor.track(const prefix: msestring;
+                                const sender: tobject; const atext: msestring);
+var
+ intf1: iassistiveclient;
+ mstr1: msestring;
+begin
+ if getcorbainterface(sender,typeinfo(iassistiveclient),intf1) then begin
+  track(prefix,intf1,atext);
+ end
+ else begin
+  mstr1:= '';
+  if sender is tcomponent then begin
+   mstr1:= tcomponent(sender).name+' ';
+  end;
+  track(prefix,iassistiveclient(nil),mstr1+atext);
+ end;
+end;
+
 procedure tassistivemonitor.doenter(const sender: iassistiveclient);
 begin
- track(sender,'<doenter>');
+ track('<doenter>',sender,'');
 end;
 
 procedure tassistivemonitor.clientmouseevent(const sender: iassistiveclient;
                var info: mouseeventinfoty);
 begin
- track(sender,'<clientmouseevent '+getenumname(typeinfo(eventkindty),
-                                              ord(info.eventkind))+'>');
+ track('<clientmouseevent '+getenumname(typeinfo(eventkindty),
+                       ord(info.eventkind))+'>',sender,'');
 end;
 
-procedure tassistivemonitor.track(const sender: iassistiveclient;
-                                               const atext: msestring);
-var
- mstr1: msestring;
- inst1: tobject;               
+procedure tassistivemonitor.dofocuschanged(const oldwidget: iassistiveclient;
+               const newwidget: iassistiveclient);
 begin
- mstr1:= '<'+sender.getassistivename+'> ';
- inst1:= sender.getinstance();
- if inst1 is tcomponent then begin
-  mstr1:= mstr1+tcomponent(inst1).name+' ';
- end;
- fgrid.appendrow(mstr1+atext);
- fgrid.showlastrow();
+ track('<dofocuschanged '+getname(newwidget)+'>',oldwidget,'');
+end;
+
+procedure tassistivemonitor.dokeydown(const sender: iassistiveclient;
+               const info: keyeventinfoty);
+begin
+ track('<dokeydown '+
+//          getenumname(typeinfo(eventkindty),ord(info.eventkind)) + ' ' +
+          getshortcutname(shortcutty(info.key)) + ' "'+info.chars + '" ' +
+  settostring(ptypeinfo(typeinfo(shiftstatesty)),
+                              int32(info.shiftstate),true) + ' ' +
+  settostring(ptypeinfo(typeinfo(eventstatesty)),
+                              int32(info.eventstate),true) + '>',sender,'');
+end;
+
+procedure tassistivemonitor.doactionexecute(const sender: tobject;
+               const info: actioninfoty);
+begin
+ track('<doactionexecute>',sender,'');
 end;
 
 { tmainfo }
@@ -91,6 +151,11 @@ end;
 procedure tmainfo.destroyexe(const sender: TObject);
 begin
  fmonitor.free();
+end;
+
+procedure tmainfo.exe(const sender: TObject);
+begin
+ guibeep();
 end;
 
 end.
