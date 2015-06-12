320ce09a1e634c1ee292ca0d40d5221745e0c7b8
Martin Schreiber <mse00000@gmail.com>
2015-06-12 18:12:43 +0200

    * Updated itemedit and recordfieldedit samples.

--- samples/widgets/grids/itemedit/main.pas
+++ samples/widgets/grids/itemedit/main.pas
@@ -13,6 +13,7 @@ type
    twidgetgrid1: twidgetgrid;
    itemed: titemedit;
    trealedit1: trealedit;
+   tintegeredit1: tintegeredit;
    procedure createexe(const sender: TObject);
  end;
 var
@@ -22,52 +23,10 @@ implementation
 uses
  main_mfm,msedatalist,msevaluenodes;
 
-type
- tmyitem = class(trecordvaluelistedititem)
-  private
-   frealval: real;
-   procedure setrealval(const avalue: real);
-  protected
-   procedure getvalueinfo(out atype: listdatatypety;
-                        out aindex: int32; out avaluead: pointer); override;
-   procedure setvalue(const atype: listdatatypety;
-       const aindex: int32; const getvaluemethod: getvaluemethodty); override;
-  public
-   property realval: real read frealval write setrealval;
- end;
-  
-{ tmyitem }
-
-procedure tmyitem.setrealval(const avalue: real);
-begin
- if frealval <> avalue then begin
-  frealval:= avalue;
-  valuechange();
- end;
-end;
-
-procedure tmyitem.getvalueinfo(out atype: listdatatypety; out aindex: int32;
-                                                        out avaluead: pointer);
-begin
- atype:= dl_real;
- aindex:= 0;
- avaluead:= @frealval;
-end;
-
-procedure tmyitem.setvalue(const atype: listdatatypety;
-               const aindex: int32; const getvaluemethod: getvaluemethodty);
-var
- rea1: realty;
-begin
- if (atype = dl_real) and (aindex = 0) then begin
-  getvaluemethod(rea1);
-  realval:= rea1;
- end;
-end;
-
 procedure tmainfo.createexe(const sender: TObject);
 begin
- itemed.itemlist.add(tmyitem.create);
+ itemed.itemlist.add(trealvaluelistedititem.create);
+ itemed.itemlist.add(tintegervaluelistedititem.create);
 end;
 
 end.
