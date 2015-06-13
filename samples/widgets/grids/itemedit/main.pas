26ae8f9b6570f9cbe9d49cfe4ac2f9f3a36abc9e
Martin Schreiber <mse00000@gmail.com>
2015-06-13 20:35:05 +0200

    * Updated itemedit samples.

--- samples/widgets/grids/itemedit/main.pas
+++ samples/widgets/grids/itemedit/main.pas
@@ -6,7 +6,7 @@ uses
  msegraphics,msegraphutils,mseevent,mseclasses,msewidgets,mseforms,msedataedits,
  mseedit,msegrids,mseificomp,mseificompglob,mseifiglob,msestatfile,msestream,
  msestrings,msewidgetgrid,sysutils,msedatanodes,mselistbrowser,mserealsumedit,
- msesimplewidgets;
+ msesimplewidgets,msegraphedits,msescrollbar;
 
 type
  tmainfo = class(tmainform)
@@ -14,6 +14,17 @@ type
    itemed: titemedit;
    trealedit1: trealedit;
    tintegeredit1: tintegeredit;
+   tdatetimeedit1: tdatetimeedit;
+   tstringedit1: tstringedit;
+   tbutton1: tbutton;
+   tbutton2: tbutton;
+   tbooleanedit1: tbooleanedit;
+   twidgetgrid2: twidgetgrid;
+   tprogressbar1: tprogressbar;
+   tdatabutton1: tdatabutton;
+   tslider2: tslider;
+   tslider1: tslider;
+   tdatabutton2: tdatabutton;
    procedure createexe(const sender: TObject);
  end;
 var
@@ -25,8 +36,21 @@ uses
 
 procedure tmainfo.createexe(const sender: TObject);
 begin
- itemed.itemlist.add(trealvaluelistedititem.create);
- itemed.itemlist.add(tintegervaluelistedititem.create);
+{
+ itemed.itemlist.add(trealvaluelistedititem.create());
+ itemed.itemlist.add(tintegervaluelistedititem.create());
+ itemed.itemlist.add(tstringvaluelistedititem.create());
+ itemed.itemlist.add(tdatetimevaluelistedititem.create());
+ itemed.itemlist.add(tbooleanvaluelistedititem.create());
+}
+ itemed.itemlist.add(trealvaluelistedititem.create(0));
+ itemed.itemlist.add(tintegervaluelistedititem.create(1));
+ itemed.itemlist.add(tstringvaluelistedititem.create(2));
+ itemed.itemlist.add(tdatetimevaluelistedititem.create(3));
+ itemed.itemlist.add(tbooleanvaluelistedititem.create(4));
+ itemed.itemlist.add(trealvaluelistedititem.create(5));
+ itemed.itemlist.add(trealvaluelistedititem.create(5));
+
 end;
 
 end.
