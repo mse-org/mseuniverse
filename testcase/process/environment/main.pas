8cb9f9c304bffad2e0438818922404641b614e23
Martin Schreiber <mse00000@gmail.com>
2015-07-05 16:25:10 +0100

    * process testcase.

--- testcase/process/environment/main.pas
+++ testcase/process/environment/main.pas
@@ -7,7 +7,8 @@ uses
  mseprocess,msesimplewidgets,msewidgets,msedataedits,mseedit,msegrids,
  mseificomp,mseificompglob,mseifiglob,msestatfile,msestream,msestrings,msetypes,
  msewidgetgrid,sysutils,mseterminal,msegraphedits,msescrollbar,msetimer,
- msedispwidgets,mserichstring;
+ msedispwidgets,mserichstring,msebitmap,msedatanodes,msefiledialog,
+ mselistbrowser,msesys;
 
 type
  tmainfo = class(tmainform)
@@ -19,9 +20,8 @@ type
    par: tstringgrid;
    env: tstringgrid;
    shell: tbooleanedit;
+   commandline: tfilenameedit;
    procedure exe(const sender: TObject);
-   procedure receiveexe(const sender: TObject; var atext: AnsiString;
-                   const errorinput: Boolean);
  end;
 var
  mainfo: tmainfo;
@@ -30,15 +30,8 @@ uses
  main_mfm,msefileutils{$ifdef mswindows},windows{$endif};
  
 procedure tmainfo.exe(const sender: TObject);
-var
- fna1: filenamety;
 begin
  term.clear();
-{$ifdef mswindows}
- fna1:= filepath('testproc/testproc.exe');
-{$else}
- fna1:= filepath('testproc/testproc');
-{$endif}
  if shell.value then begin
   term.optionsprocess:= term.optionsprocess + [pro_shell];
   term.optionsprocess:= term.optionsprocess - [pro_noshell];
@@ -47,16 +40,8 @@ begin
   term.optionsprocess:= term.optionsprocess - [pro_shell];
   term.optionsprocess:= term.optionsprocess + [pro_noshell];
  end;
- term.execprog(fna1,'',par.datacols[0].datalist.asarray,
+ term.execprog(commandline.value,'',par.datacols[0].datalist.asarray,
                                    env.datacols[0].datalist.asarray);
-end;
-
-procedure tmainfo.receiveexe(const sender: TObject; var atext: AnsiString;
-               const errorinput: Boolean);
-begin
-{$ifdef mswindows}
-// oemtochar(pchar(atext),pchar(atext));
-{$endif}
 end;
 
 end.
--- testcase/process/environment/testproc/testproc.pas
+++ testcase/process/environment/testproc/testproc.pas
@@ -3,7 +3,52 @@ program testproc;
 {$ifdef mswindows}{$apptype console}{$endif}
 uses
  {$ifdef FPC}{$ifdef unix}cthreads,cwstring,{$endif}{$endif}
- sysutils;
+ sysutils,msestrings{$ifdef mswindows},windows{$endif};
+
+{$ifdef mswindows}
+function getenvironmentvariablecount(): int32;
+var
+ po1,po2: pchar;
+ i1: int32;
+begin
+ po1:= getenvironmentstrings();
+ po2:= po1;
+ i1:= 0;
+ while po2^ <> #0 do begin
+  while po2^ <> #0 do begin
+   inc(po2);
+  end;
+  inc(po2);
+  inc(i1);
+ end; 
+ freeenvironmentstrings(po1);
+ result:= i1;
+end;
+              //fpc version is buggy
+function getenvironmentstring(aindex: int32): string;
+var
+ po1,ps,pe: pchar;
+begin
+ result:= '';
+ po1:= getenvironmentstrings();
+ ps:= po1;
+ pe:= po1;
+ repeat
+  while pe^ <> #0 do begin
+   inc(pe);
+  end;
+  if aindex = 0 then begin
+   result:= psubstr(ps,pe);
+   break;
+  end;
+  ps:= pe+1;
+  pe:= ps;
+  dec(aindex)
+ until (ps^ = #0);
+ freeenvironmentstrings(po1);
+end;
+{$endif}
+
 var
  i1: int32;
 begin
