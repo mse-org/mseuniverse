e5b4f1f6859be4916900e996434f2c02ae4ec4ae
Martin Schreiber <mse00000@gmail.com>
2015-08-27 21:05:39 +0200

    + Layout demo.

---
+++ samples/widgets/layout/main.pas
@@ -0,0 +1,34 @@
+unit main;
+{$ifdef FPC}{$mode objfpc}{$h+}{$endif}
+interface
+uses
+ msetypes,mseglob,mseguiglob,mseguiintf,mseapplication,msestat,msemenus,msegui,
+ msegraphics,msegraphutils,mseevent,mseclasses,msewidgets,mseforms,msesplitter,
+ msedataedits,mseedit,mseificomp,mseificompglob,mseifiglob,msestatfile,
+ msestream,msestrings,sysutils,msesimplewidgets;
+
+type
+ tmainfo = class(tmainform)
+   tlayouter1: tlayouter;
+   tmemoedit1: tmemoedit;
+   tstringedit1: tstringedit;
+   tbutton1: tbutton;
+   tgroupbox1: tgroupbox;
+   tspacer1: tspacer;
+   tsimplewidget1: tsimplewidget;
+   tscrollbox1: tscrollbox;
+   tsplitter1: tsplitter;
+   tstatfile1: tstatfile;
+   tmemoedit2: tmemoedit;
+   tstringedit2: tstringedit;
+   tstringedit3: tstringedit;
+   tbutton2: tbutton;
+   tbutton3: tbutton;
+   tbutton4: tbutton;
+ end;
+var
+ mainfo: tmainfo;
+implementation
+uses
+ main_mfm;
+end.
