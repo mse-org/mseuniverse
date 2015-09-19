0be7d18e26e215956a778c5fdd175af91881636a
Martin Schreiber <mse00000@gmail.com>
2015-09-19 16:30:16 +0200

    + Simple MySQL example.

---
+++ samples/db/simplemysql/main.pas
@@ -0,0 +1,26 @@
+unit main;
+{$ifdef FPC}{$mode objfpc}{$h+}{$endif}
+interface
+uses
+ msetypes,mseglob,mseguiglob,mseguiintf,mseapplication,msestat,msemenus,msegui,
+ msegraphics,msegraphutils,mseevent,mseclasses,msewidgets,mseforms,msedatabase,
+ msemysqlconn,sysutils,mdb,msebufdataset,msedb,mseifiglob,msesqldb,msqldb,
+ msedataedits,msedbedit,mseedit,msegraphedits,msegrids,mseificomp,
+ mseificompglob,mselookupbuffer,msescrollbar,msestatfile,msestream,msestrings;
+
+type
+ tmainfo = class(tmainform)
+   conn: tmsemysqlconnection;
+   trans: tmsesqltransaction;
+   query: tmsesqlquery;
+   tmsedatasource1: tmsedatasource;
+   dbnav: tdbnavigator;
+   grid: tdbstringgrid;
+ end;
+var
+ mainfo: tmainfo;
+ 
+implementation
+uses
+ main_mfm;
+end.
