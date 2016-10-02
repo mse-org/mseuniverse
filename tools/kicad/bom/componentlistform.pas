e952923a922aa836ee702d161ecd4d0df34e1a9e
Martin Schreiber <mse00000@gmail.com>
2016-10-02 15:31:38 +0200

    * MSEkicadBOM component selector.

--- tools/kicad/bom/componentlistform.pas
+++ tools/kicad/bom/componentlistform.pas
@@ -20,7 +20,13 @@ type
    kinded: tdbenum64editdb;
    valuefilter: tenum64editdb;
    valueselector: tenum64editdb;
+   tenum64editdb2: tenum64editdb;
+   tenum64editdb3: tenum64editdb;
+   tenum64editdb4: tenum64editdb;
+   tenum64editdb5: tenum64editdb;
    procedure edititemev(const sender: TObject);
+   procedure rowselectev(const sender: TObject; var avalue: Int64;
+                   var accept: Boolean);
  end;
 
 implementation
@@ -30,6 +36,12 @@ uses
 procedure tcomponentlistfo.edititemev(const sender: TObject);
 begin
  tcomponenteditfo.create(nil,false).show(ml_application);
+end;
+
+procedure tcomponentlistfo.rowselectev(const sender: TObject; var avalue: Int64;
+               var accept: Boolean);
+begin
+ mainmo.stockcompqu.indexlocal[0].find([avalue],[]);
 end;
 
 end.
