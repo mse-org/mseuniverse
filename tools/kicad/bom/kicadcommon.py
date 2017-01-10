4f5b5977f569dad28e59ba3c01e405b9c8234202
Martin Schreiber <mse00000@gmail.com>
2017-01-10 09:43:35 +0100

    + MSEkicad kicadcommon.py.

---
+++ tools/kicad/bom/kicadcommon.py
@@ -0,0 +1,31 @@
+# kicadcommon
+# will be moved to MSEgui Python-component later
+#
+from pcbnew import *
+layerenums = (
+ F_Cu,In1_Cu,In2_Cu,In3_Cu,In4_Cu,In5_Cu,In6_Cu,In7_Cu,
+ In8_Cu,In9_Cu,In10_Cu,In11_Cu,In12_Cu,In13_Cu,In14_Cu,In15_Cu,In16_Cu,
+ In17_Cu,In18_Cu,In19_Cu,In20_Cu,In21_Cu,In22_Cu,In23_Cu,
+ In24_Cu,In25_Cu,In26_Cu,In27_Cu,In28_Cu,In29_Cu,In30_Cu,B_Cu,
+ B_Adhes,F_Adhes,B_Paste,F_Paste,B_SilkS,F_SilkS,B_Mask,F_Mask,
+ Dwgs_User,Cmts_User,Eco1_User,Eco2_User,Edge_Cuts,Margin,
+ B_CrtYd,F_CrtYd,B_Fab,F_Fab)
+
+layernames = (
+ 'F_Cu','In1_Cu','In2_Cu','In3_Cu','In4_Cu','In5_Cu','In6_Cu','In7_Cu','In8_Cu',
+ 'In9_Cu','In10_Cu','In11_Cu','In12_Cu','In13_Cu','In14_Cu','In15_Cu','In16_Cu',
+ 'In17_Cu','In18_Cu','In19_Cu','In20_Cu','In21_Cu','In22_Cu','In23_Cu',
+ 'In24_Cu','In25_Cu','In26_Cu','In27_Cu','In28_Cu','In29_Cu','In30_Cu','B_Cu',
+ 'B_Adhes','F_Adhes','B_Paste','F_Paste','B_SilkS','F_SilkS','B_Mask',
+ 'F_Mask','Dwgs_User','Cmts_User','Eco1_User','Eco2_User','Edge_Cuts','Margin',
+ 'B_CrtYd','F_CrtYd','B_Fab','F_Fab')
+
+drillnames = ('Drill_Map')
+DRILL_MAP = 0
+drillenums = (DRILL_MAP,0)
+
+formatenums = (
+ PLOT_FORMAT_HPGL,PLOT_FORMAT_GERBER,PLOT_FORMAT_POST,
+ PLOT_FORMAT_DXF,PLOT_FORMAT_PDF,PLOT_FORMAT_SVG)
+
+formatnames = ('HPGL','GERBER','POST','DXF','PDF','SVG')
