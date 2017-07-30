a161c60933f3b4b3165044ce8e5237b33a088554
Martin Schreiber <mse00000@gmail.com>
2017-07-30 10:04:27 +0200

    + Noise example average option.

--- samples/signal/noisegen/main.pas
+++ samples/signal/noisegen/main.pas
@@ -7,27 +7,36 @@ uses
  msestrings,msesignal,msesigaudio,msesignoise,msechartedit,msedataedits,mseedit,
  mseificomp,mseificompglob,mseifiglob,msesiggui,msestatfile,msesigfft,
  msesigfftgui,msegraphedits,msescrollbar,msedispwidgets,mserichstring,
- msesplitter,msesimplewidgets,msefilter;
+ msesplitter,msesimplewidgets,msefilter,mseact,msestream,sysutils;
 
 type
  tmainfo = class(tmainform)
    cont: tsigcontroller;
    out: tsigoutaudio;
    noise: tsignoise;
-   tsigslider1: tsigslider;
    sta: tstatfile;
+   tlayouter1: tlayouter;
+   average: tbooleanedit;
+   tlayouter2: tlayouter;
+   tsplitter1: tsplitter;
    fft: tsigscopefft;
    scope: tsigscope;
    sampcount: tslider;
-   sampcountdi: tintegerdisp;
-   tsplitter1: tsplitter;
+   tsigslider1: tsigslider;
    kinded: tenumtypeedit;
+   averagecount: tintegerdisp;
+   sampcountdi: tintegerdisp;
+   tsimplewidget1: tsimplewidget;
    procedure onclosexe(const sender: TObject);
    procedure samcountsetexe(const sender: TObject; var avalue: realty;
                    var accept: Boolean);
    procedure typinitexe(const sender: tenumtypeedit);
    procedure kindsetexe(const sender: TObject; var avalue: Integer;
                    var accept: Boolean);
+   procedure averagesetev(const sender: TObject; var avalue: Boolean;
+                   var accept: Boolean);
+   procedure buffullev(const sender: tsigsampler;
+                   const abuffer: samplerbufferty);
  end;
 var
  mainfo: tmainfo;
@@ -56,6 +65,25 @@ procedure tmainfo.kindsetexe(const sender: TObject; var avalue: Integer;
                var accept: Boolean);
 begin
  noise.kind:= noisekindty(avalue);
+end;
+
+procedure tmainfo.averagesetev(const sender: TObject; var avalue: Boolean;
+               var accept: Boolean);
+begin
+ if avalue then begin
+  fft.sampler.options:= fft.sampler.options + [sso_average];
+ end
+ else begin
+  fft.sampler.options:= fft.sampler.options - [sso_average];
+ end;
+end;
+
+procedure tmainfo.buffullev(const sender: tsigsampler;
+               const abuffer: samplerbufferty);
+begin
+ sender.lockapplication();
+ averagecount.value:= tsigsamplerfft(sender).averagecount;
+ sender.unlockapplication();
 end;
 
 end.
