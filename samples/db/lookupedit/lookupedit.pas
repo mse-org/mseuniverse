7900ac39f91b8dbb7cf02655bff8128822277362
Martin Schreiber <mse00000@gmail.com>
2017-10-17 08:45:57 +0200

    + Lookup edit example.

---
+++ samples/db/lookupedit/lookupedit.pas
@@ -0,0 +1,12 @@
+program lookupedit;
+{$ifdef FPC}{$mode objfpc}{$h+}{$endif}
+{$ifdef FPC}
+ {$ifdef mswindows}{$apptype gui}{$endif}
+{$endif}
+uses
+ {$ifdef FPC}{$ifdef unix}cthreads,{$endif}{$endif} 
+ msegui,main,loadguitemplates;
+begin
+ application.createform(tmainfo,mainfo);
+ application.run;
+end.
---
+++ samples/db/lookupedit/main.pas
@@ -0,0 +1,38 @@
+unit main;
+{$ifdef FPC}{$mode objfpc}{$h+}{$endif}
+interface
+uses
+ msetypes,mseglob,mseguiglob,mseguiintf,mseapplication,msestat,msemenus,msegui,
+ msegraphics,msegraphutils,mseevent,mseclasses,msewidgets,mseforms,mdb,mseact,
+ msedataedits,msedbedit,msedropdownlist,mseedit,msegraphedits,msegrids,
+ mseificomp,mseificompglob,mseifiglob,mselookupbuffer,msescrollbar,msestatfile,
+ msestream,sysutils,msebufdataset,msedb,mselocaldataset,msedatabase,
+ msesqlite3conn,msesqldb,msqldb,msesplitter;
+
+type
+ tmainfo = class(tmainform)
+   detailnavig: tdbnavigator;
+   detailgrid: tdbstringgrid;
+   detailds: tmsedatasource;
+   conn: tsqlite3connection;
+   trans: tmsesqltransaction;
+   detailqu: tmsesqlquery;
+   masterqu: tmsesqlquery;
+   masterds: tmsedatasource;
+   tspacer1: tspacer;
+   tsplitter1: tsplitter;
+   tstatfile1: tstatfile;
+   horzconvex: tfacecomp;
+   tsimplewidget1: tsimplewidget;
+   mastergrid: tdbwidgetgrid;
+   tdbintegeredit1: tdbintegeredit;
+   tdbenumeditdb1: tdbenumeditdb;
+   tspacer2: tspacer;
+   masternavig: tdbnavigator;
+ end;
+var
+ mainfo: tmainfo;
+implementation
+uses
+ main_mfm;
+end.
