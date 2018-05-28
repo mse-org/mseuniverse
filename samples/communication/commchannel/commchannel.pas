06a16d9f32baded54e791c7c078e1461ddc5bf52
Martin Schreiber <mse00000@gmail.com>
2018-05-28 17:16:11 +0200

    * Skin for commchannel example. * Fixed projecttree entry for portsettingsframe in commchannel example, fixes broken automatic loading.

--- samples/communication/commchannel/commchannel.pas
+++ samples/communication/commchannel/commchannel.pas
@@ -5,7 +5,7 @@ program commchannel;
 {$endif}
 uses
  {$ifdef FPC}{$ifdef unix}cthreads,{$endif}{$endif}
- msegui,mseforms,main;
+ msegui,mseforms,main,loaddefaultskin;
 begin
  application.createform(tmainfo,mainfo);
  application.run;
