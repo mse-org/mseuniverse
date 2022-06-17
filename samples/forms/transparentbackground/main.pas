3e036db17b19a707d5e7a68e1bf724cab7e4255f
fredvs <fiens@hotmail.com>
2022-06-17 19:24:58 +0200

    Added demo with window-option: wo_transparentbackground (2)

--- samples/forms/transparentbackground/main.pas
+++ samples/forms/transparentbackground/main.pas
@@ -2,12 +2,13 @@ unit main;
 {$ifdef FPC}{$mode objfpc}{$h+}{$endif}
 interface
 uses
- sysutils, classes, MClasses,msetypes, mseglob, mseguiglob, mseguiintf,
+ 
+ sysutils, classes, MClasses,msetypes, mseglob, mseguiglob, mseguiintf,msepointer,
  mseapplication, msestat, msemenus,msegui,msegraphics, msegraphutils, mseevent,
  mseclasses, msewidgets, mseforms,msesimplewidgets, msedispwidgets,
  mserichstring, mseedit, msestatfile, msestream, msedragglob, msegrids,
  msegridsglob, mseimage, msegraphedits, mseificomp, mseificompglob, mseifiglob,
-  msescrollbar;
+ msescrollbar;
  
  
 type
@@ -19,11 +20,17 @@ type
    tedit1: tedit;
    tstringgrid1: tstringgrid;
    tbooleanedit1: tbooleanedit;
+   tpaintbox2: tpaintbox;
    procedure onexec(const sender: TObject);
    procedure oncreate(const sender: TObject);
+   procedure onmousepaintbox(const sender: twidget;
+                   var ainfo: mouseeventinfoty);
  end;
 var
  mainfo: tmainfo;
+ oripoint: pointty;
+ ispressed: Boolean = False;
+  
 implementation
 uses
  main_mfm;
@@ -38,5 +45,27 @@ begin
  optionswindow := [wo_groupleader,wo_taskbar,wo_alwaysontop,wo_transparentbackground];
  SetChildBounds(sender);
 end;
+
+procedure tmainfo.onmousepaintbox(const sender: twidget;
+               var ainfo: mouseeventinfoty);
+begin
+  if ainfo.eventkind = ek_buttonpress then
+    begin
+      ispressed := True;
+      oripoint  := ainfo.pos;
+      cursor := cr_pointinghand;
+    end;
+  if ainfo.eventkind = ek_buttonrelease then
+    begin
+      ispressed := False;
+      cursor := cr_default;
+    end;
+  if (ispressed = True) and (ainfo.eventkind = ek_mousemove) then
+    begin
+      left := left + ainfo.pos.x - oripoint.x;
+      top  := top + ainfo.pos.y - oripoint.y;
+    end;
+end;
+
 
 end.
