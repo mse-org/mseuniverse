2d6f77e8de6e2877b1c4e0f3449aa22b216175bf
Artem V. Ageev <artem.v.ageev@gmail.com>
2025-05-13 21:01:16 +0300

    fix ci

--- .github/workflows/make.sh
+++ .github/workflows/make.sh
@@ -24,8 +24,10 @@ while read -r; do
                 grep --extended-regexp '(Error:|Fatal:|Linking|exitCode)'
         fi
     )
-    printf '%s\n' "${MAPFILE[@]}"
     if ((${MAPFILE[-1]##*:})); then
         exitCode+=${MAPFILE[-1]##*:}
+        printf '%s\n' "${MAPFILE[@]}"
+    else
+        printf '%s\n' "${MAPFILE[0]}"
     fi
 done < <(find '.' -type 'f' -name '*.prj')
