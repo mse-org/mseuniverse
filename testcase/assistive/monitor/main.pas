084ec36e259b43d6a8edd0a7cf836b9a16981ca3
fredvs <fiens@hotmail.com>
2025-05-14 20:41:31 +0200

    Some fixe/update to old projects.(8)

--- testcase/assistive/monitor/main.pas
+++ testcase/assistive/monitor/main.pas
@@ -1,9 +1,10 @@
 unit main;
 {$ifdef FPC}{$mode objfpc}{$h+}{$endif}
+{$interfaces corba}
 interface
 uses
  msetypes,mseglob,mseguiglob,mseguiintf,mseapplication,msestat,msemenus,msegui,
- msegraphics,msegraphutils,mseevent,mseclasses,msewidgets,mseforms,
+ msegraphics,msegraphutils,mseevent,mseclasses,msewidgets,mseforms, mseassistivehandler,
  mseassistiveserver,mseassistiveclient,msegrids,msestrings,msesimplewidgets,
  msedataedits,mseedit,mseificomp,mseificompglob,mseifiglob,msestatfile,
  msestream,sysutils,mseact,msegraphedits,msescrollbar,msetoolbar,msemenuwidgets,
@@ -20,14 +21,59 @@ type
    procedure track(const prefix: msestring; const sender: tobject; 
                                                      const atext: msestring);
     //iassistiveserver
-   procedure doenter(const sender: iassistiveclient);
-   procedure doitementer(const sender: iassistiveclient; //sender can be nil
+   procedure doapplicationactivated();
+   procedure doapplicationdeactivated();
+   procedure dowindowactivated(const sender: iassistiveclient);
+   procedure dowindowdeactivated(const sender: iassistiveclient);
+   
+   procedure dowindowclosed(const sender: iassistiveclient);
+   
+   procedure doactivate(const sender: iassistiveclient);
+   procedure dodeactivate(const sender: iassistiveclient);
+
+   procedure dodbvaluechanged(const sender: iassistiveclientdata);
+   procedure dodataentered(const sender: iassistiveclientdata);
+
+   procedure dogridbordertouched(const sender: iassistiveclientgrid;
+                                       const adirection: graphicdirectionty);
+
+   procedure doeditcharenter(const sender: iassistiveclientedit;
+                                                const achar: msestring);
+ 
+   procedure doeditchardelete(const sender: iassistiveclientedit;
+                                                const achar: msestring);
+   
+     procedure doeditwithdrawn(const sender: iassistiveclientedit);
+  procedure doeditindexmoved(const sender: iassistiveclientedit;
+                                                const aindex: int32);
+  procedure doeditinputmodeset(const sender: iassistiveclientedit;
+                                                const amode: editinputmodety);
+  procedure doedittextblock(const sender: iassistiveclientedit;
+                    const amode: edittextblockmodety; const atext: msestring);
+  procedure donavigbordertouched(const sender: iassistiveclient;
+                                       const adirection: graphicdirectionty);
+  procedure dotabordertouched(const sender: iassistiveclient;
+                                                        const adown: boolean);   
+                                                        
+    procedure doactionexecute(const sender: iassistiveclient;//sender can be nil
+                            const senderobj: tobject; const info: actioninfoty);
+  procedure doitementer(const sender: iassistiveclient;    //sender can be nil
                             const items: shapeinfoarty; const aindex: integer);
-   procedure doitementer(const sender: iassistiveclient;
+  procedure domenuactivated(const sender: iassistiveclientmenu);
+  procedure doitementer(const sender: iassistiveclientmenu;//sender can be nil
                          const items: menucellinfoarty; const aindex: integer);
-   procedure clientmouseevent(const sender: iassistiveclient;
+  procedure dodatasetevent(const sender: iassistiveclient;
+                const akind: assistivedbeventkindty;
+                                  const adataset: pointer); //tdataset
+                                                                                                
+ 
+   procedure doenter(const sender: iassistiveclient);
+ 
+   procedure doclientmouseevent(const sender: iassistiveclient;
                                            const info: mouseeventinfoty);
-   procedure dofocuschanged(const oldwidget,newwidget: iassistiveclient);
+   procedure dofocuschanged(const sender: iassistiveclient;
+                                const oldwidget,newwidget: iassistiveclient);
+   
    procedure dokeydown(const sender: iassistiveclient;
                                         const info: keyeventinfoty);
    procedure doactionexecute(const sender: tobject; const info: actioninfoty);
@@ -163,21 +209,14 @@ begin
  track('<doenter>',sender,'');
 end;
 
-procedure tassistivemonitor.doitementer(const sender: iassistiveclient;
-               const items: shapeinfoarty; const aindex: integer);
+procedure tassistivemonitor.doitementer(const sender: iassistiveclient; //sender can be nil
+                            const items: shapeinfoarty; const aindex: integer);  
 begin
  mainfo.showvalues(sender);
  track('<doitementer shape>',sender,inttostrmse(aindex));
 end;
 
-procedure tassistivemonitor.doitementer(const sender: iassistiveclient;
-                        const items: menucellinfoarty; const aindex: integer);
-begin
- mainfo.showvalues(sender);
- track('<doitementer menu>',sender,inttostrmse(aindex));
-end;
-
-procedure tassistivemonitor.clientmouseevent(const sender: iassistiveclient;
+procedure tassistivemonitor.doclientmouseevent(const sender: iassistiveclient;
                const info: mouseeventinfoty);
 begin
  if info.eventkind in [ek_clientmouseenter,ek_buttonpress] then begin
@@ -187,8 +226,8 @@ begin
                        ord(info.eventkind)))+'>',sender,'');
 end;
 
-procedure tassistivemonitor.dofocuschanged(const oldwidget: iassistiveclient;
-               const newwidget: iassistiveclient);
+ procedure tassistivemonitor.dofocuschanged(const sender: iassistiveclient;
+                                const oldwidget,newwidget: iassistiveclient);
 begin
  track('<dofocuschanged '+getname(newwidget)+'>',oldwidget,'');
 end;
@@ -223,14 +262,117 @@ end;
 
 procedure tassistivemonitor.docellevent(const sender: iassistiveclientgrid;
                const info: celleventinfoty);
+               
+var
+ aflags: assistiveflagsty;               
 begin
  track('<docellevent>',sender,msestring(getenumname(typeinfo(info.eventkind),
            int32(info.eventkind)))+' col:'+inttostrmse(info.cell.col)+
                                  ' row:'+inttostrmse(info.cell.row));
  if info.eventkind = cek_enter then begin
-  mainfo.showtext(sender.getassistivecelltext(info.cell));
+  mainfo.showtext(sender.getassistivecelltext(info.cell, aflags));
  end;
 end;
+
+// not yet used
+
+procedure tassistivemonitor.dodataentered(Const sender: iassistiveclientdata);
+begin
+end;
+
+procedure tassistivemonitor.doeditcharenter(Const sender: iassistiveclientedit;
+                               Const achar: msestring);
+begin
+end;
+procedure tassistivemonitor.doeditchardelete(Const sender: iassistiveclientedit;
+                                Const achar: msestring);
+begin
+end;
+procedure tassistivemonitor.doeditwithdrawn(Const sender: iassistiveclientedit);
+begin
+end;
+procedure tassistivemonitor.doeditindexmoved(Const sender: iassistiveclientedit;
+                                Const aindex: int32);
+begin
+end;
+procedure tassistivemonitor.doeditinputmodeset(Const sender: iassistiveclientedit;
+                                  Const amode: editinputmodety);
+begin
+end;
+procedure tassistivemonitor.doedittextblock(Const sender: iassistiveclientedit;
+                               Const amode: edittextblockmodety; Const atext: msestring);
+begin
+end;
+
+procedure tassistivemonitor.dodbvaluechanged(const sender: iassistiveclientdata);
+begin
+end;
+
+procedure tassistivemonitor.dogridbordertouched(const sender: iassistiveclientgrid;
+                                       const adirection: graphicdirectionty);
+begin
+end;
+
+procedure tassistivemonitor.dotabordertouched(const sender: iassistiveclient;
+                                                        const adown: boolean);   
+begin
+end;
+
+procedure tassistivemonitor.doitementer(const sender: iassistiveclientmenu;//sender can be nil
+                         const items: menucellinfoarty; const aindex: integer);
+begin
+end;                         
+                         
+procedure tassistivemonitor.dodatasetevent(const sender: iassistiveclient;
+                const akind: assistivedbeventkindty;
+                                  const adataset: pointer); //tdataset
+begin
+end;  
+
+procedure tassistivemonitor.dodeactivate(const sender: iassistiveclient);
+begin
+end;
+
+procedure tassistivemonitor.doapplicationactivated();
+begin
+end;
+
+procedure tassistivemonitor.doapplicationdeactivated();
+begin
+end;
+
+procedure tassistivemonitor.dowindowactivated(Const sender: iassistiveclient);
+begin
+end;
+
+procedure tassistivemonitor.dowindowdeactivated(Const sender: iassistiveclient);
+begin
+end;
+
+procedure tassistivemonitor.dowindowclosed(Const sender: iassistiveclient);
+begin
+end;
+
+procedure tassistivemonitor.doactivate(Const sender: iassistiveclient);
+begin
+end;
+
+procedure tassistivemonitor.donavigbordertouched(Const sender: iassistiveclient;
+                                    Const adirection: graphicdirectionty);
+begin
+end;
+
+procedure tassistivemonitor.doactionexecute(Const sender: iassistiveclient;//sender can be nil
+                               Const senderobj: tobject; Const info: actioninfoty);
+begin
+end;
+
+procedure tassistivemonitor.domenuactivated(Const sender: iassistiveclientmenu);
+begin
+end;
+
+
+
 
 { tmainfo }
 
