ccc4f54432886e3f1047894775afd2b61253e6c4
fredvs <fiens@hotmail.com>
2026-02-19 14:13:42 +0100

    Update Custom shape forms from bitmap vs file.

--- samples/forms/customshape/main.pas
+++ samples/forms/customshape/main.pas
@@ -2,14 +2,15 @@ unit main;
 {$ifdef FPC}{$mode objfpc}{$h+}{$endif}
 interface
 uses
- sysutils,msetypes,mseglob,mseguiglob,mseguiintf,mseapplication,msestat,msemenus,msegui,
- msegraphics,msegraphutils,mseevent,mseclasses,msewidgets,mseforms,mseimage,
- msesimplewidgets;
+ sysutils,msetypes,mseglob,mseguiglob,mseguiintf,mseapplication,msestat,
+ msemenus,msegui,msegraphics,msegraphutils,mseevent,mseclasses,msewidgets,
+ mseforms,mseimage,msesimplewidgets;
 
 type
  tmainfo = class(tmainform)
    timage1: timage;
    tbutton1: tbutton;
+   timage2: timage;
    procedure oncre(const sender: TObject);
    procedure onexec(const sender: TObject);
  end;
@@ -21,8 +22,7 @@ uses
  main_mfm;
 procedure tmainfo.oncre(const sender: TObject);
 begin
-  ordir := msestring(IncludeTrailingBackslash(ExtractFilePath(ParamStr(0))));
-mse_shapefile := './edel-bw.png';
+mse_shapebmp := timage2.bitmap;
 optionswindow := [wo_groupleader,wo_taskbar,wo_alwaysontop,wo_noframe,wo_customshape];
 end;
 
